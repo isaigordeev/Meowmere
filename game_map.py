cb65b3beb75127ce19d6904cb04ec24b6f3abea8
IsayGordeev <gordeev.ip@phystech.edu>
2020-12-04 02:58:12 +0300

    just definitions

---
+++ game_map.py
@@ -0,0 +1,13 @@
+from game_object import *
+
+
+
+pygame.display.set_caption('under_Terraria')
+
+WINDOW_SIZE = (1000, 800)
+
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+
+BACKGROUND_COLOR = (146, 244, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
